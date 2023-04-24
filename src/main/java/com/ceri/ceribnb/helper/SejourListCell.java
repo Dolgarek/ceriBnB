8329428eb29b3d371466f86f5a497fbbf83f49e3
Theo QUEZEL PERRON <explo8413@gmail.com>
2023-04-24 14:56:00 +0200

    Update code to handle img

--- src/main/java/com/ceri/ceribnb/helper/SejourListCell.java
+++ src/main/java/com/ceri/ceribnb/helper/SejourListCell.java
@@ -1,11 +1,20 @@
 package com.ceri.ceribnb.helper;
 
+import com.ceri.ceribnb.ListSejourController;
 import com.ceri.ceribnb.entity.Sejour;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class SejourListCell extends ListCell<Sejour> {
+    private ListSejourController controller;
+
+    public SejourListCell(ListSejourController controller) {
+        this.controller = controller;
+    }
+
     @Override
     protected void updateItem(Sejour sejour, boolean empty) {
         super.updateItem(sejour, empty);
@@ -14,12 +23,17 @@ public class SejourListCell extends ListCell<Sejour> {
             setText(null);
             setGraphic(null);
         } else {
+            VBox vBox = new VBox();
             HBox hBox = new HBox();
             Text nomText = new Text(sejour.getTitre());
             Text descriptifText = new Text(sejour.getDescription());
+            ImageView imageView = new ImageView(controller.getRandomImage());
+            imageView.setFitHeight(100); // Vous pouvez ajuster la taille de l'image ici
+            imageView.setFitWidth(100);
 
             hBox.getChildren().addAll(nomText, descriptifText);
-            setGraphic(hBox);
+            vBox.getChildren().addAll(hBox, imageView);
+            setGraphic(vBox);
         }
     }
 }
