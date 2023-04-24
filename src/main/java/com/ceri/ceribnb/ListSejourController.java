735df6e6c39e11aeff0dcc9690544f8b7b80a904
Theo QUEZEL PERRON <explo8413@gmail.com>
2023-04-24 14:56:00 +0200

    Update code to handle img

--- src/main/java/com/ceri/ceribnb/ListSejourController.java
+++ src/main/java/com/ceri/ceribnb/ListSejourController.java
@@ -11,12 +11,23 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import java.util.List;
 
+import javafx.scene.image.Image;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ListSejourController {
 
     @FXML
     private ListView<Sejour> sejourListView;
+    private List<Image> images = new ArrayList<>();
+    private Random random = new Random();
 
     public void initialize() {
+        for (int i = 1; i <= 9; i++) {
+            Image image = new Image(getClass().getResourceAsStream("/img/" + i + ".png"));
+            images.add(image);
+        }
+
         SejourGenerator sg = new SejourGenerator();
         List<Utilisateur> users = sg.getUtilisateurs();
         List<Sejour> test = sg.genererSejours(10000, users);
@@ -24,6 +35,10 @@ public class ListSejourController {
         ObservableList<Sejour> sejours = FXCollections.observableArrayList(test);
 
         sejourListView.setItems(sejours);
-        sejourListView.setCellFactory(sejour -> new SejourListCell());
+        sejourListView.setCellFactory(sejour -> new SejourListCell(this));
+    }
+
+    public Image getRandomImage() {
+        return images.get(random.nextInt(images.size()));
     }
 }
