42bd07ab0505c6d86e8dcf389027e6244ad26bad
Theo QUEZEL PERRON <explo8413@gmail.com>
2023-04-24 14:20:40 +0200

    Feature add login component

--- src/main/java/com/ceri/ceribnb/HelloApplication.java
+++ src/main/java/com/ceri/ceribnb/HelloApplication.java
@@ -1,20 +1,39 @@
 package com.ceri.ceribnb;
 
+import com.ceri.ceribnb.entity.Sejour;
+import com.ceri.ceribnb.entity.Utilisateur;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
+    SejourGenerator sg = new SejourGenerator();
+
+    List<Utilisateur> users = sg.getUtilisateurs();
+    List<Sejour> test = sg.genererSejours(10000, users);
+
+    //public void start(Stage stage) throws IOException {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+    public void start(Stage primaryStage) throws IOException {
+        for (Sejour s: test) {
+            System.out.println("Sejour = " + s.getTitre() + " " + s.getDescription() + " " + s.getPrix());
+        }
+        /*FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
-        stage.show();
+        stage.show();*/
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        primaryStage.setTitle("Connexion");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
